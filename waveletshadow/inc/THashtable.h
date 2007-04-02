e82baea5ac081bcc27e5581e533a186e49fa80b9
onnepoika <onnepoika@5699bb8f-202d-0410-a568-af645a74364e>
2007-04-02 13:46:56 +0000

    -Wavelet class created -Hashtable added

---
+++ waveletshadow/inc/THashtable.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <hash_map>
+#include <iostream>
+
+class TStringHasher : public stdext::hash_compare <std::string>
+	{
+	public:
+		size_t operator() (const std::string& s) const
+			{
+			size_t h = 0;
+			std::string::const_iterator p, p_end;
+			for(p = s.begin(), p_end = s.end(); p != p_end; ++p)
+				{
+				h = 31 * h + (*p);
+				}
+			return h;
+			}
+
+		bool operator() (const std::string& s1, const std::string& s2) const
+			{
+			return s1 < s2;
+			}
+	};
+
+typedef stdext::hash_map<std::string, std::string, TStringHasher> THashTable;
