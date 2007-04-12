b16359c637ef353a04e9cbc41b865233af11be0c
onnepoika <onnepoika@5699bb8f-202d-0410-a568-af645a74364e>
2007-04-12 16:34:45 +0000

    Cleared the output

--- trunk/matlab/testwave.m
+++ trunk/matlab/testwave.m
@@ -1,19 +1,31 @@
 function testwave()
 
-clc
+%clc
 
-image=[9 7 3 5]
-mult = image.*image
+%image=[9 7 3 5];
+image=[9 7 3 5 9 7 3 5 9 7 3 5 9 7 3 5 9 7 3 5 9 7 3 5 9 7 3 5 9 7 3 5];
+%image=[0 0 0 1];
+de = decomposition( image );
+re = reconstruction( de );
 
-de = decomposition( image )
-%de = sort(de,2,'descend')
-re = reconstruction( de )
+disp(['I = [',num2str(image),']'])
+disp(['to wavelet  -> WI = [',num2str(de),']']);
+disp(['reconstruct -> I_ = [',num2str(re),']']);
+disp(' ');disp(' ');
 
+mult = image.*image;
+%de_mult= ((de*sqrt(size(image,2))).*(de*sqrt(size(image,2)))) / sqrt(size(image,2));
+de_mult= de.*de;
+re_mult = reconstruction( de_mult );
 
-de_mult = decomposition( mult )
-re_mult = reconstruction( de_mult )
+disp('Multiplication:');
+disp(' ');
+disp('Expected result: (in original domain)');
+disp(['I.*I = [',num2str(mult),'] =>  sum = ',num2str(sum(mult))]);
+disp([' ( in wavelet: W(I*I)= [',num2str( decomposition(mult) ),'] )']);
+disp(' ');
+disp('Our result: (in wavelet domain)');
+disp(['WI.*WI = [',num2str(de_mult),']']);
+disp(['reconstruct ->   I_ = [',num2str(re_mult),'] =>  sum = ',num2str(sum(re_mult))]);
+disp(' ');
 
-%ratio = de_mult./de
-
-image2=[36 2 0.7071 -0.7071]
-re_ = reconstruction( image2 )
