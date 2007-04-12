8b2937c987efa978b15c4c8c8143ad229f484d7c
onnepoika <onnepoika@5699bb8f-202d-0410-a568-af645a74364e>
2007-04-12 12:15:26 +0000

    add glew

--- trunk/waveletshadow/src/main.cpp
+++ trunk/waveletshadow/src/main.cpp
@@ -8,8 +8,8 @@
 //CONSTANTS
 const int KWindowPositionX = 100;
 const int KWindowPositionY = 100;
-const int KWindowWidth = 600;
-const int KWindowHeight = 600;
+const int KWindowWidth  = 800;//1920;//800;
+const int KWindowHeight = 600;//1200;//600;
 
 
 void initGLUTOpenGL(int argc, char **argv)
@@ -20,13 +20,13 @@ void initGLUTOpenGL(int argc, char **argv)
 	glutInitWindowPosition( KWindowPositionX, KWindowPositionY );
 	glutInitWindowSize( KWindowWidth, KWindowHeight );
 	glutCreateWindow("WaveletShadows");
-	
+
 	//Set some Glut functions
 	glutKeyboardFunc( ProcessNormalKeysWithUi );
 	glutSpecialFunc(  ProcessCursorKeysWithUi );
 	glutMouseFunc(    ProcessMouseEventWithUi );
 	glutMotionFunc(   ProcessMouseMotionEventWithUi );
-	
+
 	glutDisplayFunc( RenderSceneWithRenderer );
 	glutIdleFunc(    UpdateSceneWithRenderer );
 	glutReshapeFunc( ResizeSceneWithRenderer );
@@ -36,7 +36,7 @@ void initGLUTOpenGL(int argc, char **argv)
 
 	glShadeModel(GL_SMOOTH); //GL_FLAT | GL_SMOOTH;
 	//glShadeModel( GL_FLAT ); // | GL_SMOOTH;
-	
+
 	glHint( GL_POLYGON_SMOOTH_HINT, GL_NICEST );
 	glEnable( GL_POLYGON_SMOOTH );
 
@@ -48,8 +48,11 @@ void initGLUTOpenGL(int argc, char **argv)
 	glPolygonMode (GL_FRONT, GL_FILL); // Polygon rasterization mode (polygon filled)
 	glEnable(GL_CULL_FACE); // Enable the back face culling
 
-	//glDepthRange( 0.1f, 5.0f);
+	//glDepthRange( 0, 1);
     glEnable(GL_DEPTH_TEST); // Enable the depth test (z-buffer)
+
+	GLenum err = glewInit();
+	printf("Glew Init: %s\n", (GLEW_OK==err?"OK" : "FAIL"));
 	}
 
 
