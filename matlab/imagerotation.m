cd8f9839477f76a311b2c4cdd362624994a87d7c
onnepoika <onnepoika@5699bb8f-202d-0410-a568-af645a74364e>
2007-06-04 14:15:55 +0000

    crop the cubemap

--- matlab/imagerotation.m
+++ matlab/imagerotation.m
@@ -1,7 +1,28 @@
-function imagerotation()
-
+function a=imagerotation()
+clc
 close all
 image=double(ReadPFM('church_cubemap_32.pfm'));
-imshow(image);
-title('Original');
+figure;imshow(image);title('original');
+
+%cut cubemap
+%-----------
+[h,w,colors] = size(image);
+
+facew = w/3-1;
+faceh = h/4-1;
+roof = imcrop(image,[w/3+1 0 facew faceh ]);
+%figure;imshow(roof);title('roof');
+left = imcrop(image,[0 h/4+1 facew faceh]);
+%figure;imshow(left);title('left');
+front = imcrop(image,[w/3+1 h/4+1 facew faceh]);
+%figure;imshow(front);title('front');
+right = imcrop(image,[2*(w/3)+1 h/4+1 facew faceh]);
+%figure;imshow(right);title('right');
+floor = imcrop(image,[w/3+1  2*(h/4)+1 facew faceh]);
+%figure;imshow(floor);title('floor');
+back = imcrop(image,[w/3+1 3*(h/4)+1 facew faceh]);
+%figure;imshow(back);title('back');
+
+
+
 
