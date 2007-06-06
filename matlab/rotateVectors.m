ba24de769324be992b1747c1eb01583e4c4921ea
onnepoika <onnepoika@5699bb8f-202d-0410-a568-af645a74364e>
2007-06-06 09:22:25 +0000

    rot

--- matlab/rotateVectors.m
+++ matlab/rotateVectors.m
@@ -1,4 +1,4 @@
-function uv_array = rotateVectors( angle, vec_array )
+function uv_array = rotateVectors( x,y,z, vec_array )
 
 %APPLY ROTATION
 %around y axis
@@ -8,11 +8,11 @@ function uv_array = rotateVectors( angle, vec_array )
 %R = [cos(angle) 0 -sin(angle); 0 1 0; sin(angle) 0 cos(angle)];
 %R = [cos(angle) 0 -sin(angle); 0 1 0; sin(angle) 0 cos(angle)];
 
-%Ry = [ cos(angle) 0 sin(angle); 0 1 0; -sin(angle) 0 cos(angle) ];
-Rx = [1 0 0; 0 cos(angle) -sin(angle); 0 sin(angle) cos(angle) ];
-%Rz = [ cos(angle) -sin(angle) 0;  sin(angle) cos(angle) 0;  0 0 1 ];
+Rx = [1 0 0; 0 cos(x) -sin(x); 0 sin(x) cos(x) ];
+Ry = [ cos(y) 0 sin(y); 0 1 0; -sin(y) 0 cos(y) ];
+Rz = [ cos(z) -sin(z) 0;  sin(z) cos(z) 0;  0 0 1 ];
 
 length = size( vec_array,1 );
 for i=1:length
-    uv_array(i,:) =  VectorToCube( Rx*vec_array(i, :)' );
+    uv_array(i,:) =  VectorToCube( Rz*(Ry*(Rx*vec_array(i, :)')));
 end
