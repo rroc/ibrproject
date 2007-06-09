045dcf59bb57f067b0dcee0ce4f4e87597740403
onnepoika <onnepoika@5699bb8f-202d-0410-a568-af645a74364e>
2007-06-09 00:42:54 +0000

    Rotations work!

--- trunk/matlab/createImage.m
+++ trunk/matlab/createImage.m
@@ -12,52 +12,40 @@ for y=1:length
         index = (x) + (y-1)*length;
                     
         faceid = uv(index,1);
-        
+        u = (ceil( uv(index,2)*length))+1;
+        v = (ceil( uv(index,3)*length))+1;
+            
+%         if( u~=1 && v~=1)
+%         disp(['vec: ', num2str([u v])]);
+%         end;
+
         %roof
         if(faceid == 1)
-            u = (round( uv_roof(index,2) *length))+1;
-            v = (round( uv_roof(index,3) *length))+1;
-            
             if(u>31) u=31; end
             if(v>31) v=31; end
             imageout(x,y,:) = roof( u, v, : );
         elseif(faceid == 2)
-            u = (round( uv_left(index,2) *length))+1;
-            v = (round( uv_left(index,3) *length))+1;
-            
             if(u>31) u=31; end
             if(v>31) v=31; end
             imageout(x,y,:) = left( u, v, : );
         elseif(faceid == 3)
-            u = (round( uv_front(index,2) *length))+1;
-            v = (round( uv_front(index,3) *length))+1;
-            
             if(u>31) u=31; end
             if(v>31) v=31; end
             imageout(x,y,:) = front( u, v, : );
         elseif(faceid == 4)
-            u = (round( uv_right(index,2) *length))+1;
-            v = (round( uv_right(index,3) *length))+1;
-            
             if(u>31) u=31; end
             if(v>31) v=31; end
             imageout(x,y,:) = right( u, v, : );
         elseif(faceid == 5)
-            u = (round( uv_floor(index,2) *length))+1;
-            v = (round( uv_floor(index,3) *length))+1;
-            
             if(u>31) u=31; end
             if(v>31) v=31; end
             imageout(x,y,:) = floor( u, v, : );
         elseif(faceid == 6)
-            u = (round( uv_back(index,2) *length))+1;
-            v = (round( uv_back(index,3) *length))+1;
-            
             if(u>31) u=31; end
             if(v>31) v=31; end
             imageout(x,y,:) = back( u, v, : );
         else
-            imageout(x,y,:) = [1/faceid 1/faceid 1/faceid];
+            imageout(x,y,:) = [1 1 1];%[1/faceid 1/faceid 1/faceid];
             %faceid;
         end
     end
